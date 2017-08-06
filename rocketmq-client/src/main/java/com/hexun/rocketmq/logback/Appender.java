848dd16df89b02aa9431ed54ff7f22316363b447
yuanyue <87439247@qq.com>
2017-08-06 23:03:30 +0800

    rocketmq 1.0.0

---
+++ rocketmq-client/src/main/java/com/hexun/rocketmq/logback/Appender.java
@@ -0,0 +1,210 @@
+package com.hexun.rocketmq.logback;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.status.ErrorStatus;
+import com.hexun.common.utils.IpUtils;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.logappender.common.ProducerInstance;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author yuanyue
+ */
+public class Appender extends AppenderBase<ILoggingEvent> {
+
+    /**
+     * 发送成功的计数器
+     */
+    AtomicLong successCount = new AtomicLong(0);
+
+    /**
+     * 发送失败的计数器
+     */
+    AtomicLong failCount = new AtomicLong(0);
+
+    /**
+     * message
+     */
+    BlockingQueue<Message> messagesQueue = new LinkedBlockingQueue<>(100000);
+
+    /**
+     * Message tag define
+     */
+    private String tag;
+
+    /**
+     * Whitch topic to send log messages
+     */
+    private String topic;
+
+    /**
+     * RocketMQ nameserver address
+     */
+    private String nameServerAddress;
+
+    /**
+     * Log producer send instance
+     */
+    private DefaultMQProducer producer;
+
+    /**
+     * layout
+     */
+    private Layout layout;
+
+    /**
+     * Info,error,warn,callback method implementation
+     *
+     * @param event
+     */
+    @Override
+    protected void append(ILoggingEvent event) {
+        if (!isStarted()) {
+            return;
+        }
+        String logStr = this.layout.doLayout(event);
+        try {
+            Message msg = new Message(topic, tag, logStr.getBytes());
+            msg.getProperties().put(ProducerInstance.APPENDER_TYPE, ProducerInstance.LOGBACK_APPENDER);
+            msg.getProperties().put("sip", IpUtils.getHostIP());
+            msg.getProperties().put("shost", IpUtils.getHostName());
+            messagesQueue.add(msg);
+
+            //Send message and do not wait for the ack from the message broker.
+            //producer.sendOneway(msg);
+        } catch (Exception e) {
+            addError("Could not send message in RocketmqLogbackAppender [" + name + "]. Message is : " + logStr, e);
+        }
+    }
+
+    /**
+     * Options are activated and become effective only after calling this method.
+     */
+    public void start() {
+        int errors = 0;
+
+        if (this.layout == null) {
+            addStatus(new ErrorStatus("No layout set for the RocketmqLogbackAppender named \"" + name + "\".", this));
+            errors++;
+        }
+
+        if (errors > 0 || !checkEntryConditions()) {
+            return;
+        }
+        try {
+
+            String producerGroup = "PG-" + topic;
+            producer = new DefaultMQProducer();
+            producer.setNamesrvAddr(nameServerAddress);
+            producer.setVipChannelEnabled(false);
+            producer.setProducerGroup(producerGroup);
+            producer.setSendMsgTimeout(10000);
+            producer.setRetryTimesWhenSendFailed(1);
+            producer.setCreateTopicKey(topic);
+            producer.start();
+            started = true;
+        } catch (Exception e) {
+            addError("Starting RocketmqLogbackAppender [" + this.getName()
+                    + "] nameServerAddress:" + nameServerAddress + " topic:" + topic + " " + e.getMessage());
+        }
+        if (started) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            Message message = messagesQueue.take();
+                            if (message != null) {
+                                SendResult sendResult = producer.send(message);
+                                long l = SendStatus.SEND_OK.equals(sendResult.getSendStatus()) ?
+                                        successCount.incrementAndGet() :
+                                        failCount.incrementAndGet();
+
+                            }
+                        } catch (Exception e) {
+                            failCount.incrementAndGet();
+                            addError("while take msg error", e);
+                        }
+                    }
+                }
+            }).start();
+            Timer timer = new Timer("print send result stat");
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    addInfo("ROCKETMQ日志发送状态:成功" + successCount + ",失败" + failCount);
+                }
+            }, 0, 10000);
+        }
+        if (producer != null) {
+            super.start();
+        }
+    }
+
+    /**
+     * When system exit,this method will be called to close resources
+     */
+    public synchronized void stop() {
+        // The synchronized modifier avoids concurrent append and close operations
+        if (!this.started) {
+            return;
+        }
+
+        this.started = false;
+
+        try {
+            producer.shutdown();
+        } catch (Exception e) {
+            addError("Closeing RocketmqLogbackAppender [" + this.getName()
+                    + "] nameServerAddress:" + nameServerAddress + " topic:" + topic, e);
+        }
+
+        // Help garbage collection
+        producer = null;
+    }
+
+    protected boolean checkEntryConditions() {
+        String fail = null;
+
+        if (this.topic == null) {
+            fail = "No topic";
+        }
+
+        if (fail != null) {
+            addError(fail + " for RocketmqLogbackAppender named [" + name + "].");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Set the pattern layout to format the log.
+     */
+    public void setLayout(Layout layout) {
+        this.layout = layout;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setNameServerAddress(String nameServerAddress) {
+        this.nameServerAddress = nameServerAddress;
+    }
+}
--- rocketmq-client/src/test/java/com/hexun/rocketmq/client/test/MessageProducerTest.java
+++ rocketmq-client/src/test/java/com/hexun/rocketmq/client/test/MessageProducerTest.java
@@ -4,23 +4,38 @@ import com.hexun.common.utils.DateUtils;
 import com.hexun.rocketmq.client.MessageProducer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"/*.xml"})
+@ContextConfiguration({"/application*.xml"})
 public class MessageProducerTest {
+//
+//    @Autowired
+//    MessageProducer producer;
 
-    @Autowired
-    MessageProducer producer;
+//    @Test
+//    public void testAddAppointment() throws Exception {
+//        for (int i = 0; i < 1000; i++) {
+//            System.out.println(i);
+//            String now = DateUtils.now();
+//            producer.sendAsync(i + now, i + now, now);
+//        }
+//        Thread.sleep(2000000);
+//    }
+
+
 
     @Test
-    public void testAddAppointment() throws Exception {
+    public void testlog() throws Exception {
+        Logger logger = LoggerFactory.getLogger(MessageProducerTest.class);
         for (int i = 0; i < 1000; i++) {
             System.out.println(i);
             String now = DateUtils.now();
-            producer.sendAsync(i + now, i + now, now);
+            logger.error(now);
         }
         Thread.sleep(2000000);
     }
