712734917abdc7ca6d76397795e6ca3b4071ddf0
jialing <jialing@wjl>
2017-10-26 14:00:29 +0800

    添加日志与验签功能

--- rocketmq-webapi/src/main/java/com/hexun/rocketmq/web/WebMqService.java
+++ rocketmq-webapi/src/main/java/com/hexun/rocketmq/web/WebMqService.java
@@ -2,5 +2,5 @@ package com.hexun.rocketmq.web;
 
 
 public interface WebMqService {
-    Result postMsg(String topic, String tag, String key, String body);
+    Result postMsg(String topic, String tag, String key, String body,String sign);
 }
--- rocketmq-webapi/src/main/java/com/hexun/rocketmq/web/WebMqServiceImpl.java
+++ rocketmq-webapi/src/main/java/com/hexun/rocketmq/web/WebMqServiceImpl.java
@@ -1,7 +1,10 @@
 package com.hexun.rocketmq.web;
 
 import com.alibaba.dubbo.rpc.protocol.rest.support.ContentType;
+import com.hexun.common.utils.StringUtils;
+import com.hexun.dubbo.anno.Signature;
 import com.hexun.rocketmq.client.MessageProducer;
+
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.client.producer.SendStatus;
 import org.slf4j.Logger;
@@ -14,33 +17,38 @@ import javax.ws.rs.core.MediaType;
 
 @Service
 @Path("mq")
-@Consumes({MediaType.WILDCARD, MediaType.TEXT_XML})
-@Produces({ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8})
+@Consumes({ MediaType.WILDCARD, MediaType.TEXT_XML })
+@Produces({ ContentType.APPLICATION_JSON_UTF_8, ContentType.TEXT_XML_UTF_8 })
 public class WebMqServiceImpl implements WebMqService {
-    /**
-     * Logger
-     */
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	/**
+	 * Logger
+	 */
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    /**
-     * 消息生产者
-     */
-    @Autowired
-    MessageProducer messageProducer;
-
-    @Path("post")
-    @POST
-    public Result postMsg(@FormParam("topic") String topic,
-                          @FormParam("tag") String tag,
-                          @FormParam("key") String key,
-                          @FormParam("body") String body) {
-        try {
-            SendResult sendResult = messageProducer.send(topic, key, body, tag);
-            if (sendResult != null && SendStatus.SEND_OK.equals(sendResult.getSendStatus()))
-                return Result.success(sendResult);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return Result.fail(-1, "发送失败");
-    }
+	/**
+	 * 消息生产者
+	 */
+	@Autowired
+	MessageProducer messageProducer;
+	@Signature(signKey = "Torocketmqapi")
+	@Path("post")
+	@POST
+	public Result postMsg(@FormParam("topic") String topic, @FormParam("tag") String tag, @FormParam("key") String key, @FormParam("body") String body,@FormParam("sign") String sign) {
+		try {
+			logger.info("接收到的参数为topic={},tag={},key={},body={}", topic, tag, key, body);
+			if (!StringUtils.isNoneBlank(topic) || !StringUtils.isNoneBlank(tag) || !StringUtils.isNoneBlank(key) || !StringUtils.isNoneBlank(body)) {
+				return Result.fail(-2, "参数不能为空");
+			}
+			logger.info("开始插入队列");
+			SendResult sendResult = messageProducer.send(topic, key, body, tag);
+			if (sendResult != null && SendStatus.SEND_OK.equals(sendResult.getSendStatus())) {
+				logger.info("插入队列成功sendResult={}",sendResult);
+				return Result.success(sendResult.getSendStatus());
+			}			
+		} catch (Exception e) {
+			logger.error("插入队列异常", e);
+		}
+		logger.info("插入队列失败 topic={},tag={},key={},body={}", topic, tag, key, body);
+		return Result.fail(-1, "发送失败");
+	}
 }
