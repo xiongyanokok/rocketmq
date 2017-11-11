4dd2129d9d15bd0e132e590994e66e0992937b8d
yuanyue <87439247@qq.com>
2017-11-11 12:55:07 +0800

    rocketmq console

--- deploy/console.sh
+++ deploy/console.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
-CONTAINER_NAME=rocketmq-webapi
-array=("10.4.63.105" "10.4.63.106")
+
+#!/usr/bin/env bash
+CONTAINER_NAME=rocketmq-console
+array=("10.4.63.103")
 for data in ${array[@]}
 do
     DOCKER_CMD="docker --host=${data}:2375 "
-    WAR_SOURCE=../${CI_PROJECT_ID}${CI_BUILD_REF_NAME}/${CONTAINER_NAME}.war
-    echo "WAR_SOURCE=${WAR_SOURCE}"
+    JAR_SOURCE=../${CI_PROJECT_ID}${CI_BUILD_REF_NAME}/rocketmq-console-ng-1.0.0.jar
+    echo "JAR_SOURCE=${JAR_SOURCE}"
     echo "REMOVING ${data}"
     ${DOCKER_CMD} ps -a|grep ${CONTAINER_NAME} |grep -v grep|awk '{print $1}'|xargs -i -t ${DOCKER_CMD} rm -f {}
     echo "STARTING ${data}"
     ${DOCKER_CMD} run \
-           --env SERVER_PORT=2234 \
-           --env SERVER_IP=${data} \
-           --volume /opt/docker/${CONTAINER_NAME}/tomcat/logs:/usr/local/tomcat/logs \
+           --volume /opt/docker/${CONTAINER_NAME}/tomcat/logs:/tmp \
            --name ${CONTAINER_NAME} \
-           --publish 8371:2234/tcp \
+           --publish 8080:8080/tcp \
            --expose 2234/tcp \
            --restart always \
            --detach \
-           docker-registry.hexun.com/hexunzq/tomcat8:jdk8-standard
+           java:8
     echo "COPYING ${data}"
     ${DOCKER_CMD} cp ${WAR_SOURCE} ${CONTAINER_NAME}:/usr/local/tomcat/webapps/ROOT.war
 done
